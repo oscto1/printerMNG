25082a1f3decdc02353a44a1a8e32121cbedbdf5
Oscar Castillejo <oscarcastillejo2@gmail.com>
2026-05-26 17:48:52 -0500

    Added create printer endpoint and DTO

--- PrinterMNG.Api/Program.cs
+++ PrinterMNG.Api/Program.cs
@@ -1,7 +1,11 @@
 using PrinterMNG.Api.Dtos;
 
+const string GetPrinterEndpointName = "GetPrinter";
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
 
 List<PrinterModelDto> printerModels = [
     new PrinterModelDto(1, "Toshiba", "eStudio 0000", true),
@@ -12,17 +16,21 @@ List<PrinterModelDto> printerModels = [
 // GET /printers
 app.MapGet("/printers", () => printerModels);
 
+
 // GET /printers/1
 app.MapGet("/printers/{id}", (int id) => {
-    //PrinterModelDto printerTest = 
     Console.WriteLine(printerModels.Find(printer => printer.Id == id));
     return printerModels.Find(printer => printer.Id == id); 
-});
+}).WithName(GetPrinterEndpointName);
+
 
 // POST /printers
 app.MapPost("/printers", (CreatePrinterDto newPrinter) =>
 {
+    PrinterModelDto printerToAdd = new PrinterModelDto(printerModels.Count + 1, newPrinter.Brand, newPrinter.ModelName, newPrinter.IsColorPrinter);
+    printerModels.Add(printerToAdd);
 
+    return Results.CreatedAtRoute(GetPrinterEndpointName, new { id = printerToAdd.Id }, printerToAdd);
 });
 
 app.Run();
