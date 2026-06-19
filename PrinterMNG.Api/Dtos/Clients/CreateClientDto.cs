2c04c1ca130e8692b7d4a30bbef12947ff284aa9
Oscar Castillejo <oscarcastillejo2@gmail.com>
2026-06-19 13:54:14 -0500

    Added validation to CreateClientDto

--- PrinterMNG.Api/Dtos/Clients/CreateClientDto.cs
+++ PrinterMNG.Api/Dtos/Clients/CreateClientDto.cs
@@ -1,4 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PrinterMNG.Api.Dtos.Clients;
 public record CreateClientDto(
+    [Required][StringLength(18, MinimumLength = 5)]
+    [RegularExpression(@"^\d+$")]
+    int Id,
 
+    [Required][RegularExpression(@"^[A-Za-zÀ-ÿ\s]{1,50}$")]
+    string Name,
+
+    [Required][RegularExpression(@"^3\d{9}$")]
+    string Phone,
+
+    [Required][RegularExpression(@"^[A-Za-zÀ-ÿ0-9\s#,\.-]{1,30}$")]
+    string Location
 );
